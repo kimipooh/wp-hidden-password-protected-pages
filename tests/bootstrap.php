06675a4a4cf51106dbb6dd4f33628eb38a967aa9
Kimiya Kitani <kimipooh@gmail.com>
2020-03-08 09:50:19 +0900

    fixed unit tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,7 +23,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/wp-hidden-password-protected-page.php';
+	require dirname( dirname( __FILE__ ) ) . '/wp-hidden-password-protected-pages.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
